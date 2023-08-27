107ca38ce63760f0ec64ed7945649e702e4373f5
Sam Oblad <sam.oblad@gmail.com>
2023-08-27 15:04:29 -0600

    Implemented reset button, started working on css file

--- app.py
+++ app.py
@@ -5,14 +5,22 @@ tasks = []
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        new_task = request.form.get('task')
-        tasks.append({'task' : new_task, 'completed' : False})
 
-        completed_tasks = request.form.getlist('completed')
-        for index in completed_tasks:
-            tasks[int(index)]['completed'] = True
+    if request.method == 'POST':
+        if request.form.get('reset'):
+            tasks.clear()
+        else:
+            new_task = request.form.get('task')
+            tasks.append({'task' : new_task, 'completed' : False})
+
+            completed_tasks = request.form.getlist('completed')
+            for index in completed_tasks:
+                tasks[int(index)]['completed'] = True
+
+
+
     return render_template('index.html', tasks=tasks)
+
 
 if __name__ == '__main__':
     app.run()
